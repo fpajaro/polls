5edb8413f396f47c3c0a1f228667700edeee0e9d
fpajaro <javier.pajaro@gmail.com>
2018-10-30 15:50:11 -0700

    Polls example application with spring boot security and MySQL

---
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -0,0 +1,25 @@
+package com.example.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		Jsr310JpaConverters.class
+})
+public class PollsApplication  extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PollsApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+}
